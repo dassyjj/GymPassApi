3a868001e2f6ed5429018e8da54d34069dc48c0f
dassy <developerjjonathan@gmail.com>
2023-11-05 12:51:57 -0300

    feat: install prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+// const prisma = new PrismaClient()
+
+// prisma.user.create({
+//   data: {
+
+//   }
+// })
 
 app.get('/', async (req, reply) => {
   return 'hello'
